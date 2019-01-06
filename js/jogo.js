4ce979a57f87ecc649243236b0fc1e7e10e050e7
Allyson Athyrson <aathyrson@yahoo.com.br>
2019-01-06 12:33:58 -0200

    inicio jogo mata mosca

---
+++ js/jogo.js
@@ -0,0 +1,42 @@
+var altura = 0;
+var largura = 0;
+
+function redimensionarJanela(){
+    altura = (window.innerHeight - 100);
+    largura = (window.innerWidth - 100);
+
+    console.log(altura,largura);
+}
+redimensionarJanela();
+
+function posicaoRandomica(){
+    var posicaoY = Math.floor(Math.random() * altura);
+    var posicaoX = Math.floor(Math.random() * largura);
+
+    posicaoX = posicaoX <= 60 ? 0 : posicaoX;
+    posicaoY = posicaoY <= 60 ? 0 : posicaoY;
+
+    console.log(posicaoY, posicaoX);
+
+    // html
+    var mosquito = document.createElement('img');
+        mosquito.src = 'img/mosca.png';
+        mosquito.className = tamanhoAleatorio();
+        mosquito.style.position = 'absolute';
+        mosquito.style.left = posicaoX + 'px';
+        mosquito.style.top = posicaoY + 'px';
+    document.body.appendChild(mosquito);
+}
+
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3);
+    
+    switch(classe){
+        case 0:
+            return 'mosca1';
+        case 1:
+            return 'mosca2';
+        case 2:
+            return 'mosca3';
+    }
+}
